b23e1e6b13a6922bc37cf9603de7560bb266345e
thebigg <yunior.eury@gmail.com>
2021-08-15 00:39:07 -0500

    -Add smart pointers and cmake project.

--- main.cpp
+++ main.cpp
@@ -1,71 +1,66 @@
-#define MINIAUDIO_IMPLEMENTATION
 #include "miniaudio/miniaudio.h"
 #include <QApplication>
 #include <QWidget>
+#include <memory.h>
 
 #include <stdio.h>
 
 void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount)
 {
-    ma_decoder* pDecoder = (ma_decoder*)pDevice->pUserData;
-    if (pDecoder == NULL) {
-        return;
+    // In playback mode copy data to pOutput. In capture mode read data from pInput. In full-duplex mode, both
+    // pOutput and pInput will be valid and you can move data from pInput into pOutput. Never process more than
+    // frameCount frames.
+
+    printf("sample..\n");
+
+
+}
+
+void init_audio_device_config(ma_device_config* config)
+{
+    *config = ma_device_config_init(ma_device_type_playback);
+    config->playback.format   = ma_format_f32;   // Set to ma_format_unknown to use the device's native format.
+    config->playback.channels = 2;               // Set to 0 to use the device's native channel count.
+    config->sampleRate        = 48000;           // Set to 0 to use the device's native sample rate.
+    config->dataCallback      = data_callback;   // This function will be called when miniaudio needs more data.
+
+}
+
+int init_audio_device(ma_device* device, ma_device_config* config)
+{
+//    ma_device device;
+    if (ma_device_init(NULL, config, device) != MA_SUCCESS) {
+        return -1;  // Failed to initialize the device.
     }
+//    config.pUserData         = device.pUserData;   // Can be accessed from the device object (device.pUserData).
+    ma_device_start(device);     // The device is sleeping by default so you'll need to start it manually.
+    // Do something here. Probably your program's main loop.
 
-    ma_decoder_read_pcm_frames(pDecoder, pOutput, frameCount);
+    return 0;
+}
 
-    (void)pInput;
+void quit()
+{
+    printf("quit...");
 }
 
 int main(int argc, char** argv)
 {
+    std::unique_ptr<ma_device_config> config = std::make_unique<ma_device_config>();
+    init_audio_device_config(config.get());
+
+    std::unique_ptr<ma_device> device = std::make_unique<ma_device>();
+    init_audio_device(device.get(), config.get());
+
+    config->pUserData = device->pUserData;
+
     QApplication a(argc, argv);
-    ma_result result;
-    ma_decoder decoder;
-    ma_device_config deviceConfig;
-    ma_device device;
-
-    if (argc < 2) {
-        printf("No input file.\n");
-        return -1;
-    }
-
-    result = ma_decoder_init_file(argv[1], NULL, &decoder);
-    if (result != MA_SUCCESS) {
-        return -2;
-    }
-
-    deviceConfig = ma_device_config_init(ma_device_type_playback);
-    deviceConfig.playback.format   = decoder.outputFormat;
-    deviceConfig.playback.channels = decoder.outputChannels;
-    deviceConfig.sampleRate        = decoder.outputSampleRate;
-    deviceConfig.dataCallback      = data_callback;
-    deviceConfig.pUserData         = &decoder;
-
-    if (ma_device_init(NULL, &deviceConfig, &device) != MA_SUCCESS) {
-        printf("Failed to open playback device.\n");
-        ma_decoder_uninit(&decoder);
-        return -3;
-    }
-
-    if (ma_device_start(&device) != MA_SUCCESS) {
-        printf("Failed to start playback device.\n");
-        ma_device_uninit(&device);
-        ma_decoder_uninit(&decoder);
-        return -4;
-    }
-
-    printf("Press Enter to quit...");
-    getchar();
-
-    ma_device_uninit(&device);
-    ma_decoder_uninit(&decoder);
-
     QWidget main{};
-
     main.show();
-
     a.exec();
 
-    return 0;
+    ma_device_uninit(device.get());
+
+    quit();
+   return 0;
 }
