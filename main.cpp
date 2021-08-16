d058e8622e6b297b9cd3f1082da430365689ca5b
thebigg <yunior.eury@gmail.com>
2021-08-15 23:23:04 -0500

    Add Peak Amplitude function(could be useful later).

--- main.cpp
+++ main.cpp
@@ -5,20 +5,70 @@
 
 #include <stdio.h>
 
+/**
+ * Not working properly at the moment.
+ * @brief calc_peak_amplitude
+ * @param pOutput
+ * @param pInput
+ * @param frameCount
+ * @return
+ */
+float calc_peak_amplitude(void* pOutput, const void* pInput, ma_uint32 frameCount)
+{
+    //    printf("sample..\n");
+
+    //    printf("frame count:%d\n", frameCount);
+
+        float maxValue = 0;
+        float maxAmplitude = 0x7fffffff;
+        float captureValue = 0;
+        float minAmplitude = 0;
+        uint32_t current_sample_value = 0;
+
+        const float * audioInput = static_cast<const float*>(pInput);
+
+        for (unsigned int i = 0; i < frameCount; i++) {
+
+          current_sample_value =
+              static_cast<uint32_t>(std::abs((audioInput[i]) * (0x7fffffff)));
+          maxValue =
+              current_sample_value > maxValue ? current_sample_value : maxValue;
+
+    //      printf("raw value-->%f\n", *(static_cast<const float*>(pInput)) * (0x7fffffff));
+          if(audioInput[i]>0)
+          {
+              printf("raw sample-->%f\n", audioInput[i]);
+          }
+    //      if(current_sample_value>0)
+    //      printf("current_sample_value-->%d\n", current_sample_value);
+
+        }
+
+        // Calculate the volume of the sound coming from the device.
+
+    //    printf("maxValue:%f\n", maxValue);
+
+        maxValue = std::min(maxValue, maxAmplitude);
+        captureValue = static_cast<uint32_t>(maxValue) / maxAmplitude;
+
+
+        // When we say "deviceLevel", what we really mean is Peak Amplitude.
+        float deviceLevel = captureValue - minAmplitude;
+
+    //    printf("deviceLevel:%f\n", deviceLevel);
+    //    printf("deviceLevel:%f\n", deviceLevel);
+}
+
 void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount)
 {
     // In playback mode copy data to pOutput. In capture mode read data from pInput. In full-duplex mode, both
     // pOutput and pInput will be valid and you can move data from pInput into pOutput. Never process more than
     // frameCount frames.
-
-    printf("sample..\n");
-
-
 }
 
 void init_audio_device_config(ma_device_config* config)
 {
-    *config = ma_device_config_init(ma_device_type_playback);
+    *config = ma_device_config_init(ma_device_type_capture);
     config->playback.format   = ma_format_f32;   // Set to ma_format_unknown to use the device's native format.
     config->playback.channels = 2;               // Set to 0 to use the device's native channel count.
     config->sampleRate        = 48000;           // Set to 0 to use the device's native sample rate.
@@ -28,15 +78,12 @@ void init_audio_device_config(ma_device_config* config)
 
 int init_audio_device(ma_device* device, ma_device_config* config)
 {
-//    ma_device device;
     if (ma_device_init(NULL, config, device) != MA_SUCCESS) {
         return -1;  // Failed to initialize the device.
     }
-//    config.pUserData         = device.pUserData;   // Can be accessed from the device object (device.pUserData).
-    ma_device_start(device);     // The device is sleeping by default so you'll need to start it manually.
-    // Do something here. Probably your program's main loop.
 
-    return 0;
+    ma_result res =  ma_device_start(device);     // The device is sleeping by default so you'll need to start it manually.
+    return res;
 }
 
 void quit()
@@ -52,7 +99,7 @@ int main(int argc, char** argv)
     std::unique_ptr<ma_device> device = std::make_unique<ma_device>();
     init_audio_device(device.get(), config.get());
 
-    config->pUserData = device->pUserData;
+    config->pUserData = device->pUserData; // Can be accessed from the device object (device.pUserData).
 
     QApplication a(argc, argv);
     QWidget main{};
